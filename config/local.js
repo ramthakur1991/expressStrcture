13ed8da028854b40228049e0b197178110e17198
ramthakur1991 <mss.ramthakur@gmail.com>
2016-11-24 13:17:08 +0530

    Update local.js

--- config/local.js
+++ config/local.js
@@ -1,13 +1,4 @@
 
 module.exports = {
 	port: 3000 ,
-	appSMTP: {
-		host: 'smtp.sendgrid.net',
-		port: 587,
-		debug: true,
-		auth: {
-		  user: "broadplace",
-		  pass: "broadplace*87"
-		}
-	},
 };
