cb7a497f1c2a63d12d1390f30091eb080d5b6271
parveen <mss.parveensachdeva@gmail.com>
2017-07-12 16:58:53 +0530

    update config file and package.json file

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 const config = function() {
-	this.dbNameDev= "drinker_dev" ;
-	this.dbNameProd = "drinker_prod" ;
-	this.dbNameTest = "drinker_test";
-	this.port= 3000 ;
-	this.mediaPath= "http://mastersoftwaretechnologies.com:"+ this.port ;
+	this.dbNameDev  = "" ;
+	this.dbNameProd = "" ;
+	this.dbNameTest = "";
+	this.port	= 3000 ;
+	this.mediaPath	= "http://hosturl.com:"+ this.port ;
 };
 
 module.exports = new config() ;
